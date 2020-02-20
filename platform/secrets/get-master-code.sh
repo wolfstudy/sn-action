47d435547c42bb794bce0a8b7782724a77bb6f87
xiaolong.ran <rxl@apache.org>
2020-02-20 14:39:51 +0800

    fix cp error
    Signed-off-by: xiaolong.ran <rxl@apache.org>

--- platform/secrets/get-master-code.sh
+++ platform/secrets/get-master-code.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/wolfstudy/sn-action.git
-cd sn-action
-cp -R platform/bin/ platform/bin
-cp -R platform/lib/ platform/lib
-cp -R platform/etc/ platform/etc
-cp -R platform/share/ platform/share
+cd sn-action && mkdir sn-platform
+cp -R platform/bin/ sn-platform/bin
+cp -R platform/lib/ sn-platform/lib
+cp -R platform/etc/ sn-platform/etc
+cp -R platform/share/ sn-platform/share
 
